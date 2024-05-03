d8d23f7fa7a1dc73543e05b4a9e6608b14351119
Khai Nguyen <tkhai12386@gmail.com>
2024-05-03 19:33:01 +0700

    Update User Interface

--- QuanLiThuvien/Models/UserModel.java
+++ QuanLiThuvien/Models/UserModel.java
@@ -1,47 +1,87 @@
 package QuanLiThuvien.Models;
 
-import java.util.Arrays;
-import java.util.Map;
-
-import javax.swing.JOptionPane;
+import java.util.*;
 
 public class UserModel extends Model {
-    public UserModel() {
-        table = "users";
-        fillable = Arrays.asList("username", "password", "email", "role", "infomation");
+
+    public UserModel(String table) {
+        setNameTable(table);
+        setFillable(setFillTable(table));
     }
 
-    public static int checkAccount(String user, String pass) {
-        UserModel userModel = new UserModel();
-        for (Map<String,String> string : userModel.getData()) {
-            if (string.get("username").toString().equals(user) && string.get("password").equals(pass)) {
-                //System.out.println(string.get("role"));
-                return Integer.valueOf(string.get("role"));
-            }
-        }
+    public List<String> setFillTable(String table) {
+        List<String> fillTable;
+        fillTable = switch (table) {
+            case "users" -> Arrays.asList("id", "username", "password", "email", "phone", "role", "name", "sex", "create_day", "ip", "birthday");
+            case "products" -> Arrays.asList("id", "category_id", "name", "quantity", "price", "type_product", "image", "description");
+            case "categorys" -> Arrays.asList("id", "name_category", "image_category");
+            default -> null;
+        };
+        return fillTable;
+    }
 
-        return 3;
+    public static List<Map<String, String>> getData(String table) {
+        UserModel userModel = new UserModel(table);
+        return userModel.getData();
+    }
+
+    public static List<Map<String, String>> getDataByID(String table, int ID) {
+        UserModel userModel = new UserModel(table);
+        return userModel.getDataByID(ID);
+    }
+
+    public static int getIDByUser(String table, String user) {
+        UserModel userModel = new UserModel(table);
+        return Integer.parseInt(userModel.getDataByUser(user, "id"));
+    }
+
+    public static Boolean checkAccount(String user, String pass) {
+        UserModel userModel = new UserModel("users");
+        return userModel.checkDataExist("username", user) && userModel.getDataByUser(user, "password").equals(pass);
+    }
+
+    public static void updateData(String table, Map<String, String> dataUpdate, int ID) {
+        UserModel userModel = new UserModel(table);
+        userModel.updateByID(dataUpdate, ID);
+    }
+
+    public static Boolean createData(Map<String, String> data, String table) {
+        UserModel userModel = new UserModel(table);
+        return userModel.create(data);
+    }
+
+    public static boolean checkDataAccount(String key, String value) {
+        UserModel userModel = new UserModel("users");
+        return userModel.checkDataExist(key, value);
+    }
+
+    public static void updateInfo(String newPassword, String email) {
+        UserModel userModel = new UserModel("users");
+        userModel.updateInfomation("password", newPassword, email);
+    }
+
+    public static List<Map<String, String>> setDataForTable(String table) {
+        String query = "SELECT `categorys`.* FROM categorys WHERE 1 ORDER BY `id` ASC";
+        UserModel userModel = new UserModel(table);
+        return userModel.getDataLinkColumn(query);
+    }
+    public static List<Map<String, String>> setDataForTableBooks(String table, int id) {
+        UserModel userModel = new UserModel(table);
+        String query = "SELECT `products`.* FROM products WHERE products.category_id = %d;".formatted(id);
+        return userModel.getDataLinkColumn(query);
+    }
+
+    public static Boolean delete(int ID) {
+        UserModel userModel = new UserModel("products");
+        return userModel.deleteDataByID(ID);
+    }
+
+    public static int getCountProduct(int ID) {
+        UserModel userModel = new UserModel("products");
+        return userModel.getCountData(ID);
     }
 
     public static void main(String[] args) {
-        UserModel userModel = new UserModel();
-        System.out.println(userModel.getFillable().toString());
+
     }
-
-    public static Boolean checkData(String key, String value) {
-        UserModel userModel = new UserModel();
-        return userModel.checkDataExis(key, value);
-    }
-
-    public static void updateInfo(String key, String value, String email) {
-        UserModel userModel = new UserModel();
-        if (key.equals("username")) {
-            JOptionPane.showMessageDialog(null, "Không thể thay đổi tài khoản");
-        } else {
-            userModel.updateInfomation(key, value, email);
-        }
-        
-    }
-
-
 }
